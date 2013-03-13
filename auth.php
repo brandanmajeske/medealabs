48cf9476f14a5c673b8ee2c3916a596e0f587406
Brandan Majeske <bmajeske@me.com>
2013-03-12 20:13:17 -0600

    responsiveness framework check

---
+++ auth.php
@@ -0,0 +1,14 @@
+
+<?php include('views/header.inc');?>
+
+ 	<form class="form-signin">
+        <h2 class="form-signin-heading">Please sign in</h2>
+        <input type="text" class="input-block-level" placeholder="Email address">
+        <input type="password" class="input-block-level" placeholder="Password">
+        <label class="checkbox">
+          <input type="checkbox" value="remember-me"> Remember me
+        </label>
+        <button class="btn btn-large btn-warning" type="submit">Sign in</button>
+      </form>
+
+<?php include('views/footer.inc');?>
---
+++ index.php
@@ -0,0 +1,8 @@
+<?php include('views/header.inc'); ?>
+
+<h2>Hello World!</h2>
+<div class="span4">
+<p>Today is: <?php echo date('D, F Y');?></p>
+</div>
+
+<?php include('views/footer.inc'); ?>
