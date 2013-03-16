500146f67be0cbf9a677037c33fe72aec4d6fe0f
Brandan Majeske <bmajeske@me.com>
2013-03-15 19:30:52 -0600

    added more info to the item detail view

--- controllers/ItemViewController.php
+++ controllers/ItemViewController.php
@@ -9,9 +9,9 @@ require_once('models/View.php');
 $model = new ItemViewModel(MY_DSN, MY_USER, MY_PASS);
 $view = new View();
 
-$username = $_SESSION['username'];
+$username = ucfirst($_SESSION['username']);
 
 
-$view->show('header');
-echo '<h2>The Item View Page for '.ucfirst($username).'</h2>'; // Soon to be replaced by model->function action
+$view->show('header'); // Soon to be replaced by model->function action
+$view->show('details', $username);
 $view->show('footer');
